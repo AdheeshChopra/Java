bb8893f8069e12924c8fab7c985d9744ed552597
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-04-11 22:13:55 +0530

    Add files via upload

---
+++ ConditionalStatements/IfElse/IfElseAge.java
@@ -0,0 +1,14 @@
+package ConditionalStatements.IfElse;
+import java.util.*;
+public class IfElseAge {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+}
