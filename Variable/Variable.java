7da4321981c40e6f1568bfb31b300f649b9303c0
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-04-11 22:12:18 +0530

    Add files via upload

---
+++ Variable/Variable.java
@@ -0,0 +1,16 @@
+package Variable;
+
+public class Variable {
+    public static void main(String[] args){
+//        String name="Adheesh";
+//        int age = 20;
+        int a= 25;
+        int b=10;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+    }
+}
