68e1f6bc7a064d947386569522314838a48b0de0
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-04-22 22:00:09 +0530

    Add files via upload

---
+++ ConditionalStatements/Switch/Calculator.java
@@ -0,0 +1,34 @@
+package ConditionalStatements.Switch;
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("1. Press 1 for Addition\n" +
+                "2. Press 2 for Subtraction\n" +
+                "3. Press 3 for Multiplication\n" +
+                "4. Press 4 for Division\n" +
+                "5. Press 5 for Modulus");
+        int x = sc.nextInt();
+        switch(x){
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                System.out.println(a/b);
+                break;
+            case 5:
+                System.out.println(a%b);
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+}
