0752c3ee8a21a9763963035e9822bf98357dde8e
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-09-08 16:57:44 +0530

    Add files via upload

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,11 @@ public class FizzBuzz {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number: ");
-        int Number = sc.nextInt();
+        int number = sc.nextInt();
 
+        if (number%5==0 && number%3==0) System.out.println("FizzBuzz");
+        else if(number%5==0) System.out.println("Fizz");
+        else if(number%3==0) System.out.println("Buzz");
+        else System.out.println(number);
     }
 }
